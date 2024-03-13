60ed0ed08246ad13e131199aa7f1ebb1226ab56b
João Feitoza Bisneto <joaofeitoza.13@gmail.com>
2024-03-13 12:42:20 -0300

    test: ensure new command works

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,35 +7,41 @@ runner = CliRunner()
 
 
 def test_escala_cli_deve_retornar_0_ao_stdout():
-    result = runner.invoke(app, ["escala"])
+    result = runner.invoke(app, ['escala'])
     assert result.exit_code == 0
 
 
-@mark.parametrize("nota", ["C", "D", "E", "F", "G", "A", "B"])
+@mark.parametrize('nota', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
 def test_escala_cli_deve_conter_as_notas_na_resposta(nota):
-    result = runner.invoke(app, ["escala"])
+    result = runner.invoke(app, ['escala'])
     assert nota in result.stdout
 
 
-@mark.parametrize("nota", ["F", "G", "A", "A#", "C", "D", "E"])
+@mark.parametrize('nota', ['F', 'G', 'A', 'A#', 'C', 'D', 'E'])
 def test_escala_cli_deve_conter_as_notas_na_resposta_de_fa(nota):
-    result = runner.invoke(app, ["escala", "F"])
+    result = runner.invoke(app, ['escala', 'F'])
     assert nota in result.stdout
 
 
-@mark.parametrize("grau", ["I", "II", "III", "IV", "V", "VI", "VII"])
+@mark.parametrize('grau', ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
 def test_escala_cli_deve_conter_todos_os_graus(grau):
-    result = runner.invoke(app, ["escala", "F"])
+    result = runner.invoke(app, ['escala', 'F'])
     assert grau in result.stdout
 
 
-@mark.parametrize("nota", ["C", "E", "G"])
+@mark.parametrize('nota', ['C', 'E', 'G'])
 def test_acorde_cli_deve_conter_as_notas_na_resposta(nota):
-    result = runner.invoke(app, ["acorde"])
+    result = runner.invoke(app, ['acorde'])
     assert nota in result.stdout
 
 
-@mark.parametrize("grau", ["I", "III", "V"])
+@mark.parametrize('grau', ['I', 'III', 'V'])
 def test_acorde_cli_deve_conter_todos_os_graus(grau):
-    result = runner.invoke(app, ["acorde", "F"])
+    result = runner.invoke(app, ['acorde', 'F'])
     assert grau in result.stdout
+
+
+@mark.parametrize('cifra', ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'B°'])
+def test_campo_harmonico_cli_deve_conter_todas_as_cifras(cifra):
+    result = runner.invoke(app, ['campo-harmonico', 'C'])
+    assert cifra in result.stdout
